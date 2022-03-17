d91e9ff1eb44c08d36e5a6071d72da4c928b7cf4
canglevan87 <98739709+canglevan87@users.noreply.github.com>
2022-03-17 18:08:48 +0700

    init

---
+++ src/main/java/cybersoft/javabackend/java16giracanglv/Java16GiraCanglvApplication.java
@@ -0,0 +1,14 @@
+package cybersoft.javabackend.java16giracanglv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java16GiraCanglvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java16GiraCanglvApplication.class, args);
+
+	}
+
+}
