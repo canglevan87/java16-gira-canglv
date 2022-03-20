639c7b42a09ab3dae3254bbaf2ae67fc193e61de
canglevan87 <98739709+canglevan87@users.noreply.github.com>
2022-03-20 17:46:25 +0700

    Update RolesController.java

--- src/main/java/cybersoft/javabackend/java16giracanglv/controller/RolesController.java
+++ src/main/java/cybersoft/javabackend/java16giracanglv/controller/RolesController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/roles")
@@ -28,7 +30,14 @@ public class RolesController {
       return new ResponseEntity<>(roles, HttpStatus.OK);
     }
     @PostMapping
-    public Object createRoles(@Valid @RequestBody GiraRoleDTO giraRoleDTO){
+    public Object createRoles(@Valid @RequestBody GiraRoleDTO giraRoleDTO , BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors()
+                    .stream().map(t -> t.getDefaultMessage())
+                    .collect(Collectors.toList())
+                    , HttpStatus.BAD_REQUEST);
+        }
+
         GiraRoleDTO body = giraRoleService.save(giraRoleDTO);
         return new ResponseEntity<>(body, HttpStatus.CREATED);
     }
